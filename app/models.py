ab9821d4e402958b6333a4829124b7375881ea86
Neeraj <ny.neeraj12121@gmail.com>
2017-01-09 23:15:41 +0530

    initail commit

--- app/models.py
+++ app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+
+class image(models.Model):
+    img=models.FileField(upload_to='upload/')
--- app/views.py
+++ app/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.conf import settings
+from django.core.files.storage import FileSystemStorage
 
-# Create your views here.
+
+from .models import image
+from .forms import ImageForm
+
+
+def home(request):
+    documents = image.objects.all()
+    return render(request, 'home.html', { 'documents': documents })
+
+
+def upload(request):
+    if request.method == 'POST' and request.FILES['file']:
+        file = request.FILES['file']
+        fs = FileSystemStorage()
+        filename = fs.save(file.name, file)
+        uploaded_file_url = fs.url(filename)
+        return render(request, 'upload.html', {
+            'uploaded_file_url': uploaded_file_url
+        })
+    return render(request, 'upload.html')
+
+def model_form_upload(request):
+    if request.method == 'POST':
+        form = ImageForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = ImageForm()
+    return render(request, 'model_form_upload.html', {
+        'form': form
+})
+
+
